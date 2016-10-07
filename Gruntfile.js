60acbf74d87b1239fc0a0597475751b54cedd355
Alexander Ermoshin <sex4success@yandex.ru>
2016-10-07 20:33:35 +0300

    автоматизация

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,26 @@
 "use strict";
 
 module.exports = function(grunt) {
-  grunt.loadNpmTasks("grunt-browser-sync");
+ /* grunt.loadNpmTasks("grunt-browser-sync");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-postcss");
-  grunt.loadNpmTasks("grunt-sass");
-
+  grunt.loadNpmTasks("grunt-sass"); 
+  
+  grunt.loadNpmTasks("grunt-css-mqpacker"); 
+  grunt.loadNpmTasks("grunt-csso"); 
+  grunt.loadNpmTasks("grunt-contrib-imagemin");
+  grunt.loadNpmTasks("grunt-svgstore"); 
+  grunt.loadNpmTasks("grunt-svgmin");
+  grunt.loadNpmTasks("grunt-contrib-copy"); 
+  grunt.loadNpmTasks("grunt-contrib-clean"); */
+  
+  require("load-grunt-tasks")(grunt);
+  
   grunt.initConfig({
     sass: {
       style: {
         files: {
-          "css/style.css": "sass/style.scss"
+          "build/css/style.css": "sass/style.scss"
         }
       }
     },
@@ -25,23 +35,97 @@ module.exports = function(grunt) {
               "last 2 Firefox versions",
               "last 2 Opera versions",
               "last 2 Edge versions"
-            ]})
+            ]}),
+            require("css-mqpacker")({
+              
+            })   
           ]
         },
-        src: "css/*.css"
+        src: "build/css/*.css"
       }
     },
 
+    csso: {
+      style: {
+        options: {
+          report: "gzip"  
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]  
+        }
+      }  
+    },
+    
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png,jpg,gif}"]
+        }]
+      }  
+    },
+    
+    svgmin: {
+      symbols: {
+        files: [{
+          expand: true,
+          src: ["build/img/icons/*.svg"]
+        }]  
+      }
+    },
+    
+    svgstore: {
+      options: {
+        svg: {
+          style: "display: none"  
+        }  
+      },
+      symbols: {
+        files: {
+          "build/img/symbols.svg": ["img/icons/*.svg"]  
+        }  
+      }  
+    },
+    
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          src: [
+            "fonts/**/*.{woff,woff2}",
+            "img/**",
+            "js/**",
+            "*.html"
+          ],
+          dest: "build"
+        }]  
+      },
+      html: {
+        files: [{
+          expand: true,
+          src: ["*.html"],
+          dest: "build"
+        }]  
+      }
+    },
+    
+    clean: {
+        build: ["build"]
+    },
+    
     browserSync: {
       server: {
         bsFiles: {
           src: [
-            "*.html",
-            "css/*.css"
+            "build/*.html",
+            "build/css/*.css"
           ]
         },
         options: {
-          server: ".",
+          server: "build",
           watchTask: true,
           notify: false,
           open: true,
@@ -51,15 +135,31 @@ module.exports = function(grunt) {
     },
 
     watch: {
+      html: {
+        files: ["*.html"],
+        tasks: ["copy:html"]
+      },
       style: {
         files: ["sass/**/*.{scss,sass}"],
-        tasks: ["sass", "postcss"],
+        tasks: ["sass", "postcss", "csso"],
         options: {
           spawn: false
         }
       }
     }
   });
-
+    
+  grunt.registerTask("symbols", ["svgmin", "svgstore"]);  
   grunt.registerTask("serve", ["browserSync", "watch"]);
+  
+  grunt.registerTask("build", [
+    "clean",
+    "copy",     
+    "sass",
+    "postcss",
+    "csso",
+    "symbols",
+    "imagemin"
+  ]);
+  
 };
